842c14286d7e36a37745081d0b46472b08fa7586
ZaitsevaDiana <150392034+ZaitsevaDiana@users.noreply.github.com>
2024-03-16 16:02:05 +0300

    edit/ add knopka

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
@@ -1,11 +1,20 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -14,7 +23,8 @@ import com.raywenderlich.android.lab1.router.Screen
 fun ExploreButtonsScreen() {
     Column(modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+        verticalArrangement = Arrangement.Center
+    ) {
 
         MyButton()
         MyRadioGroup()
@@ -28,6 +38,19 @@ fun ExploreButtonsScreen() {
 
 @Composable
 fun MyButton() {
+    Button(onClick = {},
+        colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.blue)),
+        border = BorderStroke(
+            1.dp,
+            color = colorResource(id = R.color.black)
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.set_button),
+           color = Color.White
+        )
+
+    }
     //TODO add your code here
 }
 
